c6b1f085283da8eb52d09ea5038414b2bf1eb0a8
Amir <amir.akhoundi85@gmail.com>
2023-12-04 12:48:17 +0200

    create three components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import Header from "./components/Header";
-import Part from "./components/Part";
 import "./App.css";
 import Total from "./components/Total";
-
+import Content from "./components/Content";
 
 function App() {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
   return (
     <>
-      <Header />
-      <Part name="Fundamentals of React" exercises={10} />
-      <Part name="Using props to pass data" exercises={7} />
-      <Part name="State of a component" exercises={14} />
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
 
-      <Total exercises1={10} exercises2={7} exercises3={14} />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </>
   );
 }
 
 export default App;
-
