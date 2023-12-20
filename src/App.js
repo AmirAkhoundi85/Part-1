c501c874225666d3e60e62bffad17ab9e54d96f0
Amir <amir.akhoundi85@gmail.com>
2023-12-20 16:29:07 +0200

    Completed 1-6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from "./components/Header";
 import "./App.css";
 import Total from "./components/Total";
 import Content from "./components/Content";
+import { useState } from "react";
+
 
 function App() {
  const course = {
@@ -20,14 +22,31 @@ function App() {
        exercises: 14,
      },
    ],
+   price:120
  };
+ const {name, parts}= course
+ const [good, setGood] = useState(0);
+ const [natural, setNatural] = useState(0);
+ const [bad, setBad] = useState(0);
+
+
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+      <Header course={name} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    <hr />
+    
+      <p>Give feedback</p>
+      <button onClick={()=>{setGood(good+1)}}>Good</button>
+      <button onClick={()=>{setNatural(natural + 1)}}>neutral</button>
+      <button onClick={()=>{setBad(bad + 1)}}>bad</button>
+      <h2>good{good}</h2>
+      <h2>natural{natural}</h2>
+      <h2>bad{bad}</h2>
+      </div>
+    
   );
 }
 
