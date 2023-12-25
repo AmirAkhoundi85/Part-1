fa8e925af11fe930dc2f59a5b21c5dadfc916a6c
Amir <amir.akhoundi85@gmail.com>
2023-12-25 13:06:24 +0200

    Completed 1-12

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import { useState } from "react";
 import Statistics from "./components/Statistics";
 
 function App() {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
   const course = {
     name: "Half Stack application development",
     parts: [
@@ -29,9 +40,11 @@ function App() {
   const [good, setGood] = useState(0);
   const [natural, setNatural] = useState(0);
   const [bad, setBad] = useState(0);
-
   const [totalFeedback, setTotalFeedback] = useState(0);
   const [totalScore, setTotalScore] = useState(0);
+  const [currentAnecdote, setCurrentAnecdote]= useState(" ")
+
+  
 
   const handleFeedback = (score) => {
     setTotalFeedback(totalFeedback + 1);
@@ -47,6 +60,10 @@ function App() {
     const result = totalScore / totalFeedback;
     return Math.round(result * 100) / 100;
   };
+  const randomHandler = ()=>{
+     const randomIndex = Math.floor(Math.random() * anecdotes.length);
+     setCurrentAnecdote(anecdotes[randomIndex]);
+  }
 
   return (
     <div>
@@ -91,6 +108,10 @@ function App() {
         positivePercentage={calculatePositivePercentage()}
         average={calculateAvrage()}
       />
+      <div>
+        <p>{currentAnecdote}</p>
+        <button onClick={randomHandler}>Next anecdotes</button>
+      </div>
     </div>
   );
 }
